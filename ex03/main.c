175b9a6d743ea25f22cf8f6c5d908b553d0d8250
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-08-29 11:56:00 -0300

    Ex03

---
+++ ex03/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+
+#include <stdio.h>
+
+int main()
+{
+  setlocale(LC_ALL, "portuguese");
+  char caracter;
+  printf("Digite um caracter: ");
+  scanf("%c", &caracter);
+  printf("O código ASCII em decimal é %d\n", caracter);
+  printf("O código ASCII em octal é %o\n", caracter);
+  printf("O código ASCII em hexadecimal é %x\n", caracter);
+  
+  return 0;
+}
